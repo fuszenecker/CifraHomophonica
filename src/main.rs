4a3bf8ab0303a13888cddf33610aeaef9236842a
Robert Fuszenecker <rofu01@betssongroup.com>
2019-09-17 12:47:17 +0200

    First working version that does something.

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::collections::HashMap;
+
+use std::process;
+
+fn text_to_stat(file_name: &str) -> std::io::Result<HashMap<char, usize>> {
+    use std::io::BufReader;
+    use std::io::BufRead;
+    use std::fs::File;
+
+    let mut frequencies = HashMap::new();
+
+    let file = File::open(&file_name)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    return Ok(frequencies);
+}
+
+fn main() {
+    println!("Salve, usator! Cifra Homomorphica te salutat.");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 2 {
+        let filename = &args[1];
+        let frequencies = text_to_stat(&filename);
+
+        match frequencies {
+            Ok(freqs) => println!("Frequentia littararum: {:?}", freqs),
+            Err(error) => println!("Frequenciam littararum calculare non possum, quia {}", error)
+        }
+    } else {
+        println!("Necesse est dare (ut argumentum primum) nomen eius ducumenti quod basis calculationis frequentiae litterarum sit.");
+        process::exit(100);
+    }
+}
